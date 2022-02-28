4831fc5bb88b4863d9105b6e6b998f3e714c6223
Philipe Monterrazo <philipemonterrazo@outlook.com>
2022-02-28 20:55:02 -0300

    Varias Alterações Atrasadas PCA; PLP

---
+++ PCA - Planilha de Cobrança dos Clientes Ativos/PCA_ABC.sql
@@ -0,0 +1,85 @@
+Select A.VDPEDIPE_CODCLI AS 'COD.CLIENTE', 
+
+SUM(
+      CASE
+         WHEN A.VDPEDIPE_CODR = 904502 
+         OR 
+         A.VDPEDIPE_CODR = 903431 
+         OR 
+         A.VDPEDIPE_CODR = 903061
+         OR 
+         A.VDPEDIPE_CODR = 900090
+         OR 
+         A.VDPEDIPE_CODR = 903046
+         OR 
+         A.VDPEDIPE_CODR = 902411
+         
+         THEN A.VDPEDIPE_PREPRDT
+   END) AS '600ML',
+
+   SUM(
+      CASE
+         WHEN A.VDPEDIPE_CODR = 903482 
+
+         THEN A.VDPEDIPE_PREPRDT
+   END) AS 'HEIN.600ML',
+
+   SUM(
+      CASE
+         WHEN A.VDPEDIPE_CODR = 903129 
+         OR 
+         A.VDPEDIPE_CODR = 902311 
+         OR 
+         A.VDPEDIPE_CODR = 902451
+         
+         THEN A.VDPEDIPE_PREPRDT
+   END) AS '300ML',
+
+   SUM(
+      CASE
+         WHEN A.VDPEDIPE_CODR = 904213 
+         OR 
+         A.VDPEDIPE_CODR = 901133 
+         OR 
+         A.VDPEDIPE_CODR = 902432
+
+         THEN A.VDPEDIPE_PREPRDT
+   END) AS '1L',
+
+   SUM(
+      CASE
+         WHEN A.VDPEDIPE_CODR = 100559 
+         OR 
+         A.VDPEDIPE_CODR = 100579 
+         OR 
+         A.VDPEDIPE_CODR = 171690
+
+         THEN A.VDPEDIPE_PREPRDT
+   END) AS 'REFR. PEQ',
+
+    SUM(
+      CASE
+         WHEN A.VDPEDIPE_CODR = 198579 
+         OR 
+         A.VDPEDIPE_CODR = 50473 
+         OR 
+         A.VDPEDIPE_CODR = 50475 
+         OR 
+         A.VDPEDIPE_CODR = 171620 
+         OR 
+         A.VDPEDIPE_CODR = 99900018 
+         OR 
+         A.VDPEDIPE_CODR = 197061 
+         OR 
+         A.VDPEDIPE_CODR = 197196 
+         OR 
+         A.VDPEDIPE_CODR = 33422 
+         THEN A.VDPEDIPE_PREPRDT
+   END) AS 'REFR. GRANDE'
+
+
+from pedit01 as A
+
+where A.vdpedipe_nit > 202111010001
+
+Group by A.VDPEDIPE_CODCLI, A.VDPEDIPE_CODR, VDPEDIPE_PREPRDT ,A.vdpedipe_nit
